2fe3a0e267651affc29f3d5a2aa031a8ac2f04b0
Bark Ruffalo <hype@trulyadog.com>
2024-12-14 00:32:41 +0100

    hide Debug in production

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
+  pageExtensions: process.env.NODE_ENV === 'production' 
+    ? ['js', 'jsx', 'ts', 'tsx'].map(ext => `(?<!debug.)${ext}`)
+    : ['js', 'jsx', 'ts', 'tsx'],
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
