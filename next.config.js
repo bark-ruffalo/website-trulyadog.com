73e2f744ee3cc58ecacbb675be673c75741d8fcb
Bark Ruffalo <hype@trulyadog.com>
2024-12-14 00:24:03 +0100

    reverted next.config.js to how it was

--- next.config.js
+++ next.config.js
@@ -9,15 +9,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
-  webpack: (config, { dev }) => {
+  webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
-    if (!dev) {
-      config.module.rules.push({
-        test: /debug[\\/].*\.(tsx|ts|jsx|js)$/,
-        loader: 'ignore-loader'
-      });
-    }
     return config;
   },
 };
