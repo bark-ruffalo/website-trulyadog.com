21363dbd203bbfa90d5712f51d0a369b7707ca51
LuckyToFortune <rodrigonuevo41@gmail.com>
2024-12-13 16:55:50 +0200

    fix: balanceCard

--- app/migration/_components/Migrate/BalanceCard.tsx
+++ app/migration/_components/Migrate/BalanceCard.tsx
@@ -3,7 +3,7 @@ const BalanceCard = ({ title, balance }: { title: string; balance: string }) => 
     <div className="flex flex-col justify-between items-start p-6 min-h-[120px] flex-1 relative bg-blue-500 bg-opacity-10 rounded-2xl shadow-md overflow-hidden">
       <div className="absolute inset-0 bg-blue-500 bg-opacity-20 backdrop-blur-sm"></div>
       <div className="relative z-10 text-white">
-        <div className="text-sm font-semibold uppercase mb-1 leading-4">{title}</div>
+        <div className="text-sm font-semibold mb-1 leading-4">{title}</div>
         <div className="text-2xl font-light leading-4 pt-6">{balance}</div>
       </div>
     </div>
