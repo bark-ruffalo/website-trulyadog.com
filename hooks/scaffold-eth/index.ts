342c944c13dae441a9d2abb944b9416291e716d2
LuckyToFortune <rodrigonuevo41@gmail.com>
2024-12-13 12:02:39 +0200

    Update pawsy price

--- hooks/scaffold-eth/index.ts
+++ hooks/scaffold-eth/index.ts
@@ -3,6 +3,7 @@ export * from "./useContractLogs";
 export * from "./useDeployedContractInfo";
 export * from "./useFetchBlocks";
 export * from "./useInitializeNativeCurrencyPrice";
+export * from "./useInitializePawsyPrice";
 export * from "./useNetworkColor";
 export * from "./useOutsideClick";
 export * from "./useScaffoldContract";
---
+++ hooks/scaffold-eth/useInitializePawsyPrice.ts
@@ -0,0 +1,32 @@
+import { useCallback, useEffect } from "react";
+import { useTargetNetwork } from "./useTargetNetwork";
+import { useInterval } from "usehooks-ts";
+import scaffoldConfig from "~~/scaffold.config";
+import { useGlobalState } from "~~/services/store/store";
+import { fetchPriceFromUniswap } from "~~/utils/scaffold-eth";
+
+const enablePolling = false;
+
+/**
+ * Get the price of PAWSY based on PAWSY/DAI trading pair from Uniswap SDK
+ */
+export const useInitializePawsyPrice = () => {
+  const setPawsyPrice = useGlobalState(state => state.setPawsyPrice);
+  const setIsPawsyFetching = useGlobalState(state => state.setIsPawsyFetching);
+  const { targetNetwork } = useTargetNetwork();
+
+  const fetchPrice = useCallback(async () => {
+    setIsPawsyFetching(true);
+    const price = await fetchPriceFromUniswap(targetNetwork);
+    setPawsyPrice(price);
+    setIsPawsyFetching(false);
+  }, [setIsPawsyFetching, setPawsyPrice, targetNetwork]);
+
+  // Get the price of PAWSY from Uniswap on mount
+  useEffect(() => {
+    fetchPrice();
+  }, [fetchPrice]);
+
+  // Get the price of PAWSY from Uniswap at a given interval
+  useInterval(fetchPrice, enablePolling ? scaffoldConfig.pollingInterval : null);
+};
