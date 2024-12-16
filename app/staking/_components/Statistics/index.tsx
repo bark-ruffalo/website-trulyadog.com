473ca4af9bb0cd053ee215832180aebbe51550c1
LuckyToFortune <rodrigonuevo41@gmail.com>
2024-12-16 21:08:44 +0200

    Update: replace approve with stake after approve, restore statistics panel

--- app/staking/_components/Statistics/index.tsx
+++ app/staking/_components/Statistics/index.tsx
@@ -1,51 +1,46 @@
 import { Card } from "./Card";
-
-// import { formatEther } from "viem";
-// import { useAccount } from "wagmi";
-
-// import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
+import { formatEther } from "viem";
+import { useAccount } from "wagmi";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 export function Statistics() {
-  // const account = useAccount();
+  const account = useAccount();
 
-  // const { data: totalStaking } = useScaffoldReadContract({
-  //   contractName: "StakingVault",
-  //   functionName: "getTotalStakedAmount",
-  // }) as { data: bigint };
+  const { data: totalStaking } = useScaffoldReadContract({
+    contractName: "StakingVault",
+    functionName: "getTotalStakedAmount",
+  }) as { data: bigint };
 
-  // const { data: totalUsers } = useScaffoldReadContract({
-  //   contractName: "StakingVault",
-  //   functionName: "getTotalLockedUsers",
-  // });
+  const { data: totalUsers } = useScaffoldReadContract({
+    contractName: "StakingVault",
+    functionName: "getTotalLockedUsers",
+  });
 
-  // const { data: totalRewards } = useScaffoldReadContract({
-  //   contractName: "StakingVault",
-  //   functionName: "getLifetimeRewards",
-  //   args: [account.address],
-  // });
+  const { data: totalRewards } = useScaffoldReadContract({
+    contractName: "StakingVault",
+    functionName: "getLifetimeRewards",
+    args: [account.address],
+  });
 
-  // const { data: rewardTokenSymbol } = useScaffoldReadContract({
-  //   contractName: "RewardToken",
-  //   functionName: "symbol",
-  // });
+  const { data: rewardTokenSymbol } = useScaffoldReadContract({
+    contractName: "RewardToken",
+    functionName: "symbol",
+  });
 
   const cards = [
     {
       title: "TOTAL VALUE LOCKED",
-      // value: `${totalStaking ? formatEther(totalStaking) : 0}`,
-      value: "0",
+      value: `${totalStaking ? formatEther(totalStaking) : 0}`,
       className: "green",
     },
     {
       title: "TOTAL STAKERS",
-      // value: `${totalUsers}`,
-      value: "0",
+      value: `${totalUsers}`,
       className: "green",
     },
     {
       title: "LIFETIME REWARDS",
-      // value: `${totalRewards ? totalRewards : 0} ${rewardTokenSymbol}`,
-      value: "0",
+      value: `${totalRewards ? totalRewards : 0} ${rewardTokenSymbol}`,
       className: "green",
     },
   ];
