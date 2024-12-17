d1bca133faf697748499862e29e66794361d493f
Bark Ruffalo <hype@trulyadog.com>
2024-12-17 09:57:01 +0100

    improve UI for portfolio cards

--- app/staking/_components/Portfolio/Card.tsx
+++ app/staking/_components/Portfolio/Card.tsx
@@ -20,8 +20,8 @@ export function PortfolioCard({ item }: { item: CardProps }) {
   const lastClaimDate = new Date(Number(item.lastClaimTime) * 1000); // Convert seconds to milliseconds
 
   // Format the dates
-  const unlockDateString = unlockDate.toLocaleDateString() + " " + unlockDate.toLocaleTimeString();
-  const lastClaimDateString = lastClaimDate.toLocaleDateString() + " " + lastClaimDate.toLocaleTimeString();
+  const unlockDateString = unlockDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
+  // const lastClaimDateString = lastClaimDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' }) + " " + lastClaimDate.toLocaleTimeString();
 
   const onUnstake = async () => {
     try {
@@ -39,33 +39,33 @@ export function PortfolioCard({ item }: { item: CardProps }) {
   return (
     <div className="flex flex-col justify-between p-5 min-h-[120px] max-w-[380px] bg-base-200 dark:bg-[#8d54751a] rounded-lg relative flex-grow m-1 box-border">
       <div className="flex flex-col mb-2 gap-2 w-full">
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between w-full gap-8">
           <span className="text-base-content/70 dark:text-[#b2bfce] font-light">Pool Index</span>
           <span className="text-base-content dark:text-white font-light">{item.poolId.toString()}</span>
         </div>
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between w-full gap-8">
           <span className="text-base-content/70 dark:text-[#b2bfce] font-light">Staked Amount</span>
           <span className="text-base-content dark:text-white font-light">{formatEther(item.amount)}</span>
         </div>
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between w-full gap-8">
           <span className="text-base-content/70 dark:text-[#b2bfce] font-light">Lock Period</span>
           <span className="text-base-content dark:text-white font-light">
             {convertSecondsToDays(Number(item.lockPeriod))} Days
           </span>
         </div>
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between w-full gap-8">
           <span className="text-base-content/70 dark:text-[#b2bfce] font-light">Unlocked At</span>
           <span className="text-base-content dark:text-white font-light">{unlockDateString}</span>
         </div>
-        {/* <div className="flex justify-between w-full">
+        {/* <div className="flex justify-between w-full gap-8">
           <span className="text-base-content/70 dark:text-[#b2bfce] font-light">Last Claim Time</span>
           <span className="text-base-content dark:text-white font-light">{lastClaimDateString}</span>
         </div> */}
-        {/* <div className="flex justify-between w-full">
+        {/* <div className="flex justify-between w-full gap-8">
           <span className="text-[#b2bfce] font-light">Staking Token</span>
           <Address address={item.stakingToken} />
         </div> */}
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between w-full gap-8">
           <span className="text-base-content/70 dark:text-[#b2bfce] font-light">Lock Status</span>
           <span className="text-base-content dark:text-white font-light">{item.isLocked ? "Locked" : "Unlocked"}</span>
         </div>
