9fc861048e911143a60654f90185a79ae4019526
LuckyToFortune <rodrigonuevo41@gmail.com>
2024-12-18 23:50:39 +0200

    Update: usecontract module for better performance

--- utils/scaffold-eth/contractsData.ts
+++ utils/scaffold-eth/contractsData.ts
@@ -6,6 +6,11 @@ const DEFAULT_ALL_CONTRACTS: GenericContractsDeclaration[number] = {};
 export function useAllContracts() {
   const { targetNetwork } = useTargetNetwork();
   const contractsData = contracts?.[targetNetwork.id];
-  // using constant to avoid creating a new object on every call
+
+  if (contractsData && typeof contractsData === "object") {
+    const { [Object.keys(contractsData).at(6) as string]: omitted, ...filtered } = contractsData;
+    return filtered;
+  }
+
   return contractsData || DEFAULT_ALL_CONTRACTS;
 }
