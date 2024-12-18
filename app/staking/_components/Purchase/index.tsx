af7ccc4525ee38d7c1afad0088e61888f83bde51
LuckyToFortune <rodrigonuevo41@gmail.com>
2024-12-19 00:52:55 +0200

    remove: unused codes, fix undefined variable issues

--- app/staking/_components/Purchase/index.tsx
+++ app/staking/_components/Purchase/index.tsx
@@ -9,37 +9,6 @@ interface PoolDataProps {
   isActive: boolean;
 }
 
-// const lockPeriods = [
-//   (50 * 24 * 60 * 60).toString(),
-//   (100 * 24 * 60 * 60).toString(),
-//   (200 * 24 * 60 * 60).toString(),
-//   (400 * 24 * 60 * 60).toString(),
-// ];
-
-// const staticPoolData = [
-//   {
-//     poolId: BigInt(0),
-//     isActive: true,
-//     stakingToken: "0x29e39327b5B1E500B87FC0fcAe3856CD8F96eD2a",
-//     lockPeriods,
-//     rewardRates: ["14", "55", "164", "438"],
-//   },
-//   {
-//     poolId: BigInt(1),
-//     isActive: true,
-//     stakingToken: "0x1437819DF58Ad648e35ED4f6F642d992684B2004",
-//     lockPeriods,
-//     rewardRates: ["68", "164", "383", "876"],
-//   },
-//   {
-//     poolId: BigInt(2),
-//     isActive: true,
-//     stakingToken: "0x96fc64cae162c1cb288791280c3eff2255c330a8",
-//     lockPeriods,
-//     rewardRates: ["123", "274", "602", "1314"],
-//   },
-// ];
-
 export function Purchase() {
   const { data: poolData } = useScaffoldReadContract({
     contractName: "StakingVault",
