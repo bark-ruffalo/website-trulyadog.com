3b6d9d32b322f96156df6a3452d8458ad227600a
Bark Ruffalo <hype@trulyadog.com>
2025-01-10 22:46:47 +0100

    change light theme

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     themes: [
       {
         light: {
-          ...require("daisyui/src/theming/themes")["coffee"],
+          ...require("daisyui/src/theming/themes")["cupcake"],
         },
       },
       {
